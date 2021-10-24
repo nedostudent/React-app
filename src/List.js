65722d1d61df0bba6a597b713d5d871f73f3012e
nedostudent <spicediller@mail.ru>
2021-10-24 20:12:20 +0600

    Add array list, add and remote function

--- src/List.js
+++ src/List.js
@@ -1,35 +1,93 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-export default class List extends Component {
-    constructor(){
-       super();
-       this.state={ count: 0, countAdd: 0, countDel: 0}
-   }
+class List extends Component {
+  constructor(props) {
+    super(props);
 
-  onclick(type){
-      if(type === 'add'){
-          this.setState(prevState => ({count: prevState.count + 1, countAdd: prevState.countAdd + 1}))
-      }
-      else if (type ==='sub') {
-          this.setState(prevState => ({count: prevState.count - 1, countDel: prevState.countDel + 1}))
-      }
+    this.state = {
+      list: [],
+      value: '',
+      count: 0,
+      countAdd: 0,
+      countDel: 0
+    };
   }
+  onChangeValue = event => {
+    this.setState({ value: event.target.value });
+  };
+  onAddItem = () => {
+    this.setState(state => {
+      const list = [...state.list, state.value];
 
-   render() {
+      return {
+        list,
+        value: '',
+    };
+    });
+    this.setState(prevState => ({count: prevState.count + 1, countAdd: prevState.countAdd + 1}))
+  };
+  onRemoveItem = i => {
+    this.setState(state => {
+      const list = state.list.filter((item, j) => i !== j);
+
+      return {
+        list,
+      };
+    });
+    this.setState(prevState => ({count: prevState.count - 1, countDel: prevState.countDel + 1}))
+  };
+
+  render() {
     return (
+      <div>
         <div>
-        <div>
-            Count: {this.state.count}
-            </div>
-            <div>
-            Add count: {this.state.countAdd}
-            </div>
-            <div>
-            Del count: {this.state.countDel}
-            </div>
-            <input type='button' onClick={this.onclick.bind(this, 'add')} value='Inc'/>
-            <input type='button' onClick={this.onclick.bind(this, 'sub')} value='Dec'/>
+        Кол-во элементов = {this.state.count}
         </div>
-     )
-   }
+        <div>
+        Всего было добавлено = {this.state.countAdd}
+        </div>
+        <div>
+         Всего было удалено = {this.state.countDel}
+        </div>
+          <div>
+            <ul >
+              {this.state.list.map((item, index) => (
+                <li key={item, index} >
+                  {item}
+                  <button
+                    type="button"
+                    onClick={() => this.onRemoveItem(index)}
+                    style={{
+                        margin: '10px',
+                    }}
+                  >
+                    Удалить
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+            <div>
+                <input
+                  type="text"
+                  placeholder="Введите текст"
+                  value={this.state.value}
+                  onChange={this.onChangeValue}
+                  style={{
+                      margin: '20px',
+                  }}
+                />
+                <button
+                  type="button"
+                  onClick={this.onAddItem}
+                  disabled={!this.state.value}
+                >
+                    Добавить
+                </button>
+            </div>
+      </div>
+    );
+  }
 }
+
+export default List;
